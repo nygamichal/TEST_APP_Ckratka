e13e891054fb34f58b9b88234c65841227d08169
nygamichal <mnyga94@gmail.com>
2017-06-29 08:30:30 +0200

    Dodaj pliki projektów.

---
+++ TEST_APP_Ckratka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST_APP_Ckratka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
